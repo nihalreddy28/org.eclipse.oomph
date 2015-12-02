a4c6e0db0427d8099d043fabedab5d2d08a6e561
nihalreddy28 <nihalc@jfrog.com>
2015-12-01 17:27:38 -0800

    Update .travis-deploy.sh

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -18,16 +18,16 @@ then
       cd deployment-work
 	  
       # setup git and clone from gh-pages branch
-      git config --global user.email "travis-deployer@klib.io"
+      git config --global user.email "nihalreddy28@gmail.com"
       git config --global user.name "Travis Deployer"
-      git clone --quiet --branch=gh-pages https://pekirsc:${GH_TOKEN}@github.com/peterkir/org.eclipse.oomph.git . > /dev/null 2>&1 || error_exit "Error cloning gh-pages"
+      git clone --quiet --branch=gh-pages https://nihalreddy28: b9df869b7e259c2575f5f7d558cc0c381cdc3f53@github.com/nihalreddy28/org.eclipse.oomph.git . > /dev/null 2>&1 || error_exit "Error cloning gh-pages"
     
 	  BINTRAY_URL=https://bintray.com/artifact/download/peterkir/generic/$TRAVIS_BRANCH/$TRAVIS_BUILD_NUMBER/
 	
 	  echo -e "updating index.html with build details"
 	  DATE=`date +%Y%m%d-%H%M%S`
 	  sed -i -e 's|<h1>customized oomph build - last updated on .*\?<\/h1>|<h1>customized oomph build - last updated on '$DATE'</h1>|g' index.html
-	  sed -i -e 's|<h2>Branch '$TRAVIS_BRANCH' - TravisCI build <a href="https://travis-ci.org/peterkir/org.eclipse.oomph/builds/.*\?">#.*\?</a> - build on .*\?<\/h2>|<h2>Branch '$TRAVIS_BRANCH' - TravisCI build <a href="https://travis-ci.org/peterkir/org.eclipse.oomph/builds/'$TRAVIS_BUILD_ID'">#'$TRAVIS_BUILD_NUMBER'</a> - build on '$DATE'</h2>|g' index.html
+	  sed -i -e 's|<h2>Branch '$TRAVIS_BRANCH' - TravisCI build <a href="https://travis-ci.org/nihalreddy28/org.eclipse.oomph/builds/.*\?">#.*\?</a> - build on .*\?<\/h2>|<h2>Branch '$TRAVIS_BRANCH' - TravisCI build <a href="https://travis-ci.org/peterkir/org.eclipse.oomph/builds/'$TRAVIS_BUILD_ID'">#'$TRAVIS_BUILD_NUMBER'</a> - build on '$DATE'</h2>|g' index.html
 	
       # add, commit and push files
       git add -f .
